b8ef90e2a0b83f5fb8c5fadaaeee5b3ecd54d09e
Koten <Kotenkiton@gmail.com>
2022-07-09 12:46:22 +0300

    Настройки тестов вынесены в конфигурацию. Тесты стали более гибкими. Для смены браузера зайти в конфиг и заменить значение.

--- src/test/java/io/qaguru/owner/SeleniumTest.java
+++ src/test/java/io/qaguru/owner/SeleniumTest.java
@@ -1,28 +1,41 @@
 package io.qaguru.owner;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qaguru.owner.сonfig.WebDriverProvider;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumTest {
 
+    private WebDriver driver = new WebDriverProvider().get();
+
+    //(1)Благодаря 14 строчке мне не нужны строки с 23 по 29 тесты.
+    //(2)Настройки тестов вынесены в конфигурацию.
+    //(3) Для смены браузера достаточно зайти в WebDriverConfig и заменить. Код тестов переписывать не надо.
+
 
     @Test
-    public void testGithubTitle(){
-        // Настройка chromedriver если без неё не пашет.
+    public void testGithubTitle() {
+//        // Настройка chromedriver если без неё не пашет.
+//
+//        WebDriverManager.chromedriver().setup();
+//
+//        // конфигурация запуска baseurl = github
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://github.com");
 
-        WebDriverManager.chromedriver().setup();
-
-        // конфигурация запуска baseurl = github
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://github.com");
+        // Код выполнения теста.
         String title = driver.getTitle();
-        assertEquals(title,"GitHub: Where the world builds software · GitHub");
+        assertEquals(title, "GitHub: Where the world builds software · GitHub");
         driver.quit();
-
     }
+
+    @AfterEach
+    public void stopDriver(){
+        driver.quit();
+    }
+
 }
--- src/test/java/io/qaguru/owner/сonfig/Browser.java
+++ src/test/java/io/qaguru/owner/сonfig/Browser.java
@@ -5,6 +5,4 @@ public enum Browser {
     CHROME,
 
     Firefox
-
-
 }

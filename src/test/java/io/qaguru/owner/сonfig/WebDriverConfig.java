cac94e5f244e2db7037ebe7a7213c3932f462be6
Koten <Kotenkiton@gmail.com>
2022-07-09 13:30:49 +0300

    Добавлено SystemGetProperty. через ./gradlew clean test -Dbrowser=СHROME работает только хром. Firefox не запустить.

--- src/test/java/io/qaguru/owner/сonfig/WebDriverConfig.java
+++ src/test/java/io/qaguru/owner/сonfig/WebDriverConfig.java
@@ -1,12 +1,15 @@
 package io.qaguru.owner.сonfig;
 
-public class WebDriverConfig {
 
-    public Browser getBrowser(){
-        return Browser.CHROME;
-    }
+public class WebDriverConfig {
 
     public String getBaseUrl() {
         return "https://github.com";
     }
+
+    public Browser getBrowser(){
+        String browser = System.getProperty("browser");
+        return Browser.valueOf(browser);
+    }
+
 }
